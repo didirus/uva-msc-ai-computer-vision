69ed9dad12637ba69fd417153af6c99cd88f61d2
tushar1408 <tushar.1408@gmail.com>
2017-02-17 15:28:48 +0100

    final version

--- assignment_1/photometric/get_source.m
+++ assignment_1/photometric/get_source.m
@@ -11,12 +11,15 @@ end
 
 % TODO: define arbitrary direction to V
 
+% Light sources rontal, left-above, right-above, right-below, left-below
+
 V = [0 0 -1 ; 1 1 -1; -1 1 -1; 1 -1 -1 ; -1 -1 -1] ;
 
-scriptV = zeros(5,3);
+
+
+scriptV = zeros(5,3); % Init ScriptV with zeros
 
 % TODO: normalize V into scriptV
-
 
 for x = 1:5
      normal = norm(V(x,:)) ;
