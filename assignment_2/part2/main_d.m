8ff984830774a2ceb3e2a2c4aa4d4d90d51d60d3
tushar1408 <tushar.1408@gmail.com>
2017-02-23 18:09:24 +0100

    2.4

--- assignment_2/part2/main_d.m
+++ assignment_2/part2/main_d.m
@@ -9,6 +9,15 @@
 % reference = imread('../Images/reference.png');
 % match_im = myHistMatching( input, reference );
 
-I  = imread('../Images/image3.jpeg');
-[x,y] = compute_gradient(I);
+% I  = imread('../Images/image3.jpeg');
+% [x,y] = compute_gradient(I);
 
+
+
+I  = imread('../Images/image4.jpeg');
+sigma = 50;
+kernel_size = 5;
+k=2;
+
+[x] = unsharp ( I , sigma , kernel_size , k );
+% imshow(x,[])
