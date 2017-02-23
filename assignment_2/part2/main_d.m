8a0f6ce9b249c349e3788aa8501c729e236d916e
tushar1408 <tushar.1408@gmail.com>
2017-02-23 16:33:37 +0100

    with hist matching

--- assignment_2/part2/main_d.m
+++ assignment_2/part2/main_d.m
@@ -9,4 +9,16 @@ input = imread('../Images/input.png');
 reference = imread('../Images/reference.png');
 match_im = myHistMatching( input, reference );
 
-
+figure
+subplot(3,2,1)
+imshow(input)
+subplot(3,2,2)
+histogram(input)
+subplot(3,2,3)
+imshow(reference)
+subplot(3,2,4)
+histogram(reference)
+subplot(3,2,5)
+imshow(match_im)
+subplot(3,2,6)
+histogram(match_im)
--- assignment_2/part2/myHistMatching.m
+++ assignment_2/part2/myHistMatching.m
@@ -1,50 +1,18 @@
 function imOut = myHistMatching ( input , reference )
-
-imOut = zeros(size(input))
-
-[counts_I,binLocations_I] = imhist(input) ;
-[counts_R,binLocations_R] = imhist(reference) ;
-
+[counts_I,~] = imhist(input) ;
+[counts_R,~] = imhist(reference) ;
  % cumulative distributions
- 
  c_input = cumsum(counts_I);
  c_ref = cumsum(counts_R);
- 
  %normalize
- 
  c_input = c_input ./ c_input(end) ;
- c_ref = c_ref ./ c_ref(end) ;
- 
-%  wiki : each gray level  G1 in [0,255], 
-%  we find the gray level  G2, for which  
-%  F1(G1)=F2(G2), and this is the result of histogram matching function:  
-%  M(G1)=G2, Finally, we apply the function M() 
-%  on each pixel of the reference image.
-% For us
-% M = array 
-% F1 = c_input
-% F2 = c_ref
-
-F1 = c_input ;
-F2 = c_ref;
-
-M = zeros(size(F1));
-g2 = 1;
-
-for g1 = 1:length(M)
-    flag = false;
-    while(~flag)
-        
-
-
-
-
-
- 
- 
- 
- 
- 
- imOut = 0;
+ c_ref = c_ref ./ c_ref(end);
+ %mapping
+ M = zeros(256,1,'uint8');
+ for i = 1:256
+    [~,x] = min(abs(c_input(i) - c_ref));
+    M(i) = x-1;
+ end
+ %map input image to reference image
+ imOut = M(double(input)+1); 
 end
-
