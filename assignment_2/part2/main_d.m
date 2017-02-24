2446f2057e0ef1ac939ca086b394f358d468d1b2
tushar1408 <tushar.1408@gmail.com>
2017-02-24 09:47:15 +0100

    update main again

--- assignment_2/part2/main_d.m
+++ assignment_2/part2/main_d.m
@@ -1,7 +1,10 @@
 
-test = 'denoise';
+test = 'unsharp';
+
+
+
 %2.1
-if (strcomp(test,'denoise'))
+if (strcmp(test,'denoise'))
 
     I  = imread('../Images/image2.jpeg');
     X = denoise(I,'median',[3 3]);
@@ -9,28 +12,28 @@ if (strcomp(test,'denoise'))
     imshow(X)
 end
 %2.2
-if (strcomp(test,'Histogram_matching'))
+if (strcmp(test,'Histogram_matching'))
     input = imread('../Images/input.png');
     reference = imread('../Images/reference.png');
     match_im = myHistMatching( input, reference );
 end
 %2.3
-if (strcomp(test,'compute_gradient'))
+if (strcmp(test,'compute_gradient'))
     I  = imread('../Images/image3.jpeg');
     [x,y] = compute_gradient(I);
 end
 %2.4
-if (strcomp(test,'unsharp'))
+if (strcmp(test,'unsharp'))
     I  = imread('../Images/image4.jpeg');
     sigma = 50;
-    kernel_size = 5;
+    kernel_size = 701;
     k=2;
     [x] = unsharp ( I , sigma , kernel_size , k );
 end
 
 %2.5
 
-if (strcomp(test,'LoG'))
+if (strcmp(test,'LoG'))
     lena = imread('../Images/image1.jpeg');
     figure
     subplot(131)
