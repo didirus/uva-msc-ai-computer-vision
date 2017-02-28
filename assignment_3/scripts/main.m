1e2d6e0d0239ab0d522a1506a6f128d1e5a873f6
tushar1408 <tushar.1408@gmail.com>
2017-02-28 13:00:30 +0100

    some plots not sure if its right

--- assignment_3/scripts/main.m
+++ assignment_3/scripts/main.m
@@ -1,12 +1,17 @@
-I1 = imread('../person_toy/00000001.jpg');
-% I2 = imread('../pingpong/0000.jpeg');
-%  I1 = imread('../../3.jpg');
-%  I1 = rgb2gray(I1);
-[H,~,~] = harris_corner_detector(I1);
+% I1 = imread('../person_toy/00000001.jpg');
+% % I2 = imread('../pingpong/0000.jpeg');
+% %  I1 = imread('../../3.jpg');
+% %  I1 = rgb2gray(I1);
+% [H,~,~] = harris_corner_detector(I1);
 
 % syn1 = imread('../synth1.pgm');
 % syn2 = imread('../synth2.pgm');
-% sphere1 = imread('../sphere1.ppm');
-% sphere2 = imread('../sphere2.ppm');
-% 
+% syn1 = double(syn1);
+% syn2 = double(syn2);
 % imout = lucas_kanade(15, syn1, syn2 );
+
+sphere1 = imread('../sphere1.ppm');
+sphere2 = imread('../sphere2.ppm');
+sphere1 = double(sphere1);
+sphere2 = double(sphere2);
+imout = lucas_kanade(15, sphere1, sphere2 );
