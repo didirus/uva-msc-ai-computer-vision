ef02f300d5f8fcb96930b744667e80ae42f3f664
tushar1408 <tushar.1408@gmail.com>
2017-03-07 17:30:57 +0100

    video added

--- assignment_3/scripts/make_vid.m
+++ assignment_3/scripts/make_vid.m
@@ -1,17 +1,17 @@
 %make video from sequence
-outputVideo = VideoWriter(fullfile('vector.avi'));
-outputVideo.FrameRate = 6;
+outputVideo = VideoWriter(fullfile('pingpong_vid.avi'));
+outputVideo.FrameRate = 10;
 open(outputVideo)
 
 % imageNames = dir(fullfile('../pingpong','*.jpeg'));
 % imageNames = {imageNames.name}';
 %104 or 53
-for ii = 1:103
+for ii = 1:52
     
 %     filename = ['../person_toy/00000' num2str(ii,'%03d') '.jpg'];
 %     filename = ['../pingpong/00' num2str(ii-1,'%02d') '.jpeg'];% 2d or 3d
 %     disp(num2str(ii,'%03d'))
-    filename = ['new2/' num2str(ii,'%d') '.png'];
+    filename = ['new1/' num2str(ii,'%d') '.png'];
     img = imread(filename);
     writeVideo(outputVideo,img)
 end
