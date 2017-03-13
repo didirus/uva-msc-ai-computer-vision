2ff3ed75f7adbed3f7409a9e3a34e8175ab03933
rusti <diede.rusticus@nubisonline.nl>
2017-03-13 13:24:13 +0100

    Image alignment fix

--- assignment_4/scripts/image_alignment.m
+++ assignment_4/scripts/image_alignment.m
@@ -1,6 +1,6 @@
-function [parameters, set_of_inliers] = image_alignment( image1, image2, N, P )
+function [parameters, inliers, x_win, x_tf_win, y_win, y_tf_win] = image_alignment( image1, image2, N, P )
     
-    % Converting images to grasca
+    % Converting images to grayscale
     if size(image1,3) > 1
         image1 = rgb2gray(image1);
         image2 = rgb2gray(image2);
@@ -16,7 +16,13 @@ function [parameters, set_of_inliers] = image_alignment( image1, image2, N, P )
     
     % Getting the matching keypoints
     [matches] = vl_ubcmatch(da, db);
-
+    
+    % Define the old and new x and y coordinates of the matches
+    x = fa(1, matches(1,:));
+    y = fa(2, matches(1,:));
+    x_tf_true = fb(1, matches(2,:));
+    y_tf_true = fb(2, matches (2,:));
+    
     % Keeping track of the max_inliers for N iterations
     max_inliers = 0;
     
@@ -25,26 +31,22 @@ function [parameters, set_of_inliers] = image_alignment( image1, image2, N, P )
         
         % Pick P random matches
         random_indices = randi([1 size(matches,2)],1,P);
-        picked_matches = zeros(2, P);
-        for j = 1:P
-            picked_matches(:,j) = matches(:,random_indices(j));
-        end
         
-        % Define the old and new x and y coordinates
-        x = fa(1,picked_matches(1,:));
-        y = fa(2, picked_matches(1,:));
-        x_transf = fb(1, picked_matches(2,:));
-        y_transf = fb(2, picked_matches (2,:));
+        % Define the old and new x and y coordinates for the P matches
+        x_p = x(random_indices);
+        y_p = y(random_indices); 
+        x_tf_p = x_tf_true(random_indices); 
+        y_tf_p = y_tf_true(random_indices);
         
         % Compute A and b 
         A = zeros(2*P, 6);
         b = zeros(2*P, 1);
         j = 1;
         for h = 1:2:2*P
-            A(h,:) = [ x(j) y(j) 0 0 1 0 ];
-            A(h+1,:) = [ 0 0 x(j) y(j) 0 1 ];
-            b(h,:) = x_transf(j);
-            b(h+1,:) = y_transf(j);
+            A(h,:) = [ x_p(j) y_p(j) 0 0 1 0 ];
+            A(h+1,:) = [ 0 0 x_p(j) y_p(j) 0 1 ];
+            b(h,:) = x_tf_p(j);
+            b(h+1,:) = y_tf_p(j);
             j =  j + 1;
         end
         
@@ -53,62 +55,46 @@ function [parameters, set_of_inliers] = image_alignment( image1, image2, N, P )
         M = [t(1) t(2); t(3) t(4)];
         T = [t(5); t(6)];
         
-        
-        % Computing the transformed coordinates
-        x_new = zeros(size(x));
-        y_new = zeros(size(y));
+        % Computing transformed coordinates for ALL matches
+        x_tf = zeros(size(x));
+        y_tf = zeros(size(y));
         for j = 1:size(x,2)
             temp = (M * [x(j);y(j)] + T);
-            x_new(j) = temp(1,:);
-            y_new(j) = temp(2,:);
+            x_tf(j) = temp(1,:);
+            y_tf(j) = temp(2,:);
         end
         
+        % Calculate the Euclidean distance between the ground truth and the
+        % transposed coordinates
         
-        Euclid = sqrt(((x_transf - x_new).^ 2)+((y_transf - y_new).^ 2));
-        
+        Euclid = sqrt(((x_tf_true - x_tf).^ 2)+((y_tf_true - y_tf).^ 2));
+
         if sum(Euclid<10) > max_inliers
             max_inliers = sum(Euclid<10);
             I = find(Euclid < 10);
             parameters = t;
+            winning_matches = random_indices;
             
             % Getting the coordinates of the inliers
-            set_of_inliers = zeros(size(I,2),2);
+            inliers = zeros(size(I,2),2);
             x_2 = x';
             y_2 = y';
-            set_of_inliers(:,1) = x_2(I);
-            set_of_inliers(:,2) = y_2(I);
+            inliers(:,1) = x_2(I);
+            inliers(:,2) = y_2(I);
         end
     end
-            
-        
-        % Without arrows:
-%         figure;
-%         subplot (1,2,1);
-%         imshow (uint8(im1));
-%         hold on;
-%         plot (x, y, 'b*');
-%         subplot (1,2,2);
-%         imshow (uint8(im2));
-%         hold on;
-%         plot (x_new', y_new', 'r*');
-        
-        % With arrows: 
-%         figure(2) ; clf ;
-%         imagesc(cat(2, im1, im2)) ;
-% 
-%         xa = fa(1,picked_matches(1,:)) ;
-%         xb = x_new + size(im1,2) ;
-%         ya = fa(2,picked_matches(1,:)) ;
-%         yb = y_new ;
-% 
-%         hold on ;
-%         h = line([xa ; xb], [ya ; yb]) ;
-%         set(h,'linewidth', 1, 'color', 'b') ;
-% 
-%         vl_plotframe(fa(:,picked_matches(1,:))) ;
-%         fb(1,:) = fb(1,:) + size(im1,2) ;
-%         vl_plotframe(fb(:,picked_matches(2,:))) ;
-%         axis image off ;
-
+    
+    % Define the old and new x and y coordinates of winning matches
+    x_win = x(winning_matches);
+    y_win = y(winning_matches); 
+    
+    % Computing only the transformed coordinates of the winning matches
+    x_tf_win = zeros(size(x_win));
+    y_tf_win = zeros(size(y_win));
+    for j = 1:size(x_win,2)
+        temp = (M * [x_win(j);y_win(j)] + T);
+        x_tf_win(j) = temp(1,:);
+        y_tf_win(j) = temp(2,:);
+    end
 end
 
--- assignment_4/scripts/main.m
+++ assignment_4/scripts/main.m
@@ -3,37 +3,56 @@
 % Artificial Intelligence Master
 % University of Amsterdam
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%% Select assignment and image by uncommenting the line %%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 test = 'image_alignment';
 % test = 'image_stitching';
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%% 3.1 Image Alignment %%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if (strcmp(test,'image_alignment'))
     
     im1 = imread('../boat1.pgm');
     im2 = imread('../boat2.pgm');
-    [trans_params, inliers] = image_alignment(im1, im2, 2, 4);
+    [trans_params, set_of_inliers, x, x_tf, y, y_tf] = image_alignment(im1, im2, 10, 10);
     m = reshape(trans_params(1:4),[2 2]);
     m = m';
     t = trans_params(5:6);
-    
-%     a = [m;t'];
-%     result = [a';0,0,1]';
-%     trans = maketform('affine',result);
-%     newImage = imtransform(im1,trans);
-%     imshow(newImage,[]);
-% %     imshowpair(im2,newIa);
-%     title('Built in matlab');
-    
-% % new function to be implemented    
-%     figure(3)
     [t_image] = transform(im1, m, t);
+    
+    % In build function
+    a = [m';t'];
+    result = [a';0,0,1]';
+    trans = maketform('affine',result);
+    newImage = imtransform(im1,trans);
+    
+    % Plot: with arrows between matches
+    figure(1);
+    doublefigure = cat(2, im1, im2);
+    imshow(doublefigure)
+    xa = x;
+    xb = x_tf + size(im1,2);
+    ya = y;
+    yb = y_tf;
+    hold on;
+    h = line([xa ; xb], [ya ; yb]) ;
+    set(h,'linewidth', 1, 'color', 'g') ;
+    plot(xa, ya, 'o', 'MarkerSize', 15, 'MarkerEdgeColor', 'r', 'LineWidth', 2)
+    plot(xb, yb, 'o', 'MarkerSize', 15, 'MarkerEdgeColor', 'r', 'LineWidth', 2) 
+    title('Matched points with arrows in between');
+
+    % Plot: Final result comparison
+    figure(2)
+    subplot(1,4,1)
+    imshow(im1)
+    title('Original image');
+    subplot(1,4,2)
     imshow(t_image,[]);
-%     title('custom image transform');
+    title('Custom image transform');
+    subplot(1,4,3)
+    imshow(newImage,[]);
+    title('Built in matlab function');
+    subplot(1,4,4);
+    imshow(im2);
+    title('Goal rotation');
 end
