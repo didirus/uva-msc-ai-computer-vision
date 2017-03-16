af968064b8602b6a6f16aeccab889b5fe6000288
tushar1408 <tushar.1408@gmail.com>
2017-03-16 16:45:01 +0100

    2.1 done almost. need to loop over images and concat check TODO

---
+++ FinalProject/part1/descriptors.m
@@ -0,0 +1,45 @@
+function [ f , d ] = descriptors( I, type)
+binSize = 8 ;
+magnif = 3 ;
+I = im2single(I);
+if strcmp(type,'dense')
+    if size(I,3) >1
+        I = rgb2gray(I);
+    end
+    I = im2single(I);
+    Is = vl_imsmooth(I, sqrt((binSize/magnif)^2 - .25)) ;
+    [f, d] = vl_dsift(Is, 'size', binSize) ;
+    f(3,:) = binSize/magnif ;
+    f(4,:) = 0 ;
+    
+    
+    
+end
+
+if strcmp(type,'normal')
+    if size(I,3) >1
+        I = rgb2gray(I);
+    end
+    [f1,~] = descriptors(I,'dense');
+    [f_, d_] = vl_sift(I, 'frames', f1);
+    f = f_;
+    d= d_;
+end
+
+
+if strcmp(type,'rgbSIFT')
+    [f,d] = vl_phow(I,'color','rgb');
+end
+
+
+if strcmp(type,'opponent')
+    [f,d] = vl_phow(I,'color','opponent');
+end
+
+
+if strcmp(type,'RGBSIFT')
+    
+end
+
+end
+
---
+++ FinalProject/part1/test.m
@@ -0,0 +1,48 @@
+%     clear
+        I=imread('../Caltech4/ImageData/airplanes_train/img001.jpg');
+    %     %     imshow(I,[])
+    %     %     I=single(I);
+    %
+    %     I2 = im2single(I1);
+    %     I = rgb2gray(I2);
+    %     binSize = 8 ;
+    %     magnif = 3 ;
+    %     Is = vl_imsmooth(I, sqrt((binSize/magnif)^2 - .25)) ;
+    %
+    %     % dense feat
+    %     [f, d] = vl_dsift(Is, 'size', binSize) ;
+    %     f(3,:) = binSize/magnif ;
+    %     f(4,:) = 0 ;
+    %
+    %     % normal sift.
+    %     [f_, d_] = vl_sift(I, 'frames', f) ;
+    %
+    % %     figure;
+    % %     subplot(121)
+    % %     imshow(I,[])
+    % %     subplot(122)
+    % %     imshow(unit8(Is),[])
+
+    %     if f==f_
+    %         disp('hey')
+    %     end
+
+
+%     I5=imread('../Caltech4/ImageData/airplanes_train/img001.jpg');
+%     I5 = im2single(I5);
+%     %     type for rgbsift or opponentsift
+%     type_s = 'rgb';
+%     % type_s = 'opponent';
+% 
+%     [fr,ds] = vl_phow(I5,'color',type_s);
+    
+    
+    %kmeans
+    
+    
+%     TODO: loop over images and concat the descriptors. then do the
+%     kmeans.
+    
+    [~,d] = descriptors(I,'normal');
+%     [a,b] = kmeans(single(d),400);
+
