49b2fca74a3f5554a40139359f6180b491fad177
Diede Rusticus <diede.rusticus@nubisonline.nl>
2017-03-22 18:03:47 +0100

    Started part 2

--- FinalProject/part2/finetune_cnn.m
+++ FinalProject/part2/finetune_cnn.m
@@ -1,8 +1,8 @@
 function [net, info, expdir] = finetune_cnn(varargin)
 
 %% Define options
-run(fullfile(fileparts(mfilename('fullpath')), ...
-  '..', '..', '..', 'matlab', 'vl_setupnn.m')) ;
+% run(fullfile(fileparts(mfilename('fullpath')), ...
+%   '..', '..', '..', 'matlab', 'vl_setupnn.m')) ;
 
 opts.modelType = 'lenet' ;
 [opts, varargin] = vl_argparse(opts, varargin) ;
@@ -84,7 +84,57 @@ splits = {'train', 'test'};
 
 %% TODO: Implement your loop here, to create the data structure described in the assignment
 
+n = 0;
+for s = 1:length(splits)
+    for i = 1:length(classes)
+        filename = char(strcat('../Caltech4/ImageData/', classes(i),'_', splits(s), '/'));
+        for j = 1:5
+            imagename = strcat(filename,'img',num2str(j,'%.3d'),'.jpg');
+            I = imread(imagename);
+            if size(I,3) > 1
+                n = n + 1;
+            end
+        end
+    end
+end
 
+data = zeros(32,32,3,n);
+labels = zeros(1,n);
+sets = zeros(1,n);
+ind = 1;
+
+for s = 1:length(splits)
+    for i = 1:length(classes)
+        filename = char(strcat('../Caltech4/ImageData/', classes(i),'_', splits(s), '/'));
+        for j = 1:5
+            imagename = strcat(filename,'img',num2str(j,'%.3d'),'.jpg');
+            I = imread(imagename);
+            if size(I,3) > 1
+                I = single(I);
+                I = imresize(I, [32 32]);
+                data(:,:,:,ind) = I;
+                
+                if strcmp(splits(s),splits(1))
+                    sets(ind) = 1;
+                else
+                    sets(ind) = 2;
+                end
+                
+                if strcmp(classes(i),classes(1))
+                    labels(ind) = 1;
+                elseif strcmp(classes(i),classes(2))
+                    labels(ind) = 2;
+                elseif strcmp(classes(i),classes(3))
+                    labels(ind) = 3;
+                elseif strcmp(classes(i),classes(4))
+                    labels(ind) = 4;
+                end
+                
+                ind = ind + 1;
+            end
+        end
+    end
+end
 %%
 % subtract mean
 dataMean = mean(data(:, :, :, sets == 1), 4);
