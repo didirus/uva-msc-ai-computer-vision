e732be1df19c271087f9516af3bccbac768ca647
rusti <diede.rusticus@nubisonline.nl>
2017-04-02 07:54:35 +0200

    ready to submit

--- FinalProject/part1/feature_extraction.m
+++ FinalProject/part1/feature_extraction.m
@@ -1,4 +1,4 @@
-function [ D ] = feature_extraction(imageset, d_ims, descr_type,descr_step_size)
+function [ D ] = feature_extraction(imageset, d_ims, sampler, descr_type, descr_step_size)
 
     % Define the 4 classes
     classes = {'airplanes','motorbikes','faces','cars'};
@@ -11,16 +11,16 @@ function [ D ] = feature_extraction(imageset, d_ims, descr_type,descr_step_size)
         % Use 250 images for the descriptors
         for j = 1:d_ims
             
+            % Print the progress of the descriptor
+            if j == 1
+                disp(strcat('class ', num2str(i), '..'))
+            end
+            
             imagename = strcat(filename,'img',num2str(j,'%.3d'),'.jpg');
             I = imread(imagename);
             
-            % Print the progress of the descriptor
-            if mod(j,50) == 0
-                disp(strcat(num2str(j)))
-            end
-            
             % Get the descriptors
-            [~, d] = descriptors(single(I), descr_type,descr_step_size);
+            [~, d] = descriptors(single(I), sampler, descr_type, descr_step_size);
             D = vertcat(D,d');
         end
     end
